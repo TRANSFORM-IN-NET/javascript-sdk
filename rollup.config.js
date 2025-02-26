8468135d2e3ca753aa5d67030716fe87c9fc9c7a
Tobias Thiele <thiele231@web.de>
2025-02-26 13:03:13 +0100

    🚀 Add @rollup/plugin-replace and update yarn.lock

--- rollup.config.js
+++ rollup.config.js
@@ -1,6 +1,7 @@
 import typescript from "rollup-plugin-typescript2";
 import commonjs from "@rollup/plugin-commonjs";
 import resolve from "@rollup/plugin-node-resolve";
+import replace from "@rollup/plugin-replace";
 
 export default {
   input: "src/index.ts",
@@ -18,6 +19,12 @@ export default {
   plugins: [
     resolve(),
     commonjs(),
+    replace({
+      preventAssignment: true,
+      values: {
+        "process.env.NODE_ENV": JSON.stringify(process.env.NODE_ENV),
+      },
+    }),
     typescript({
       tsconfigOverride: {
         compilerOptions: {
